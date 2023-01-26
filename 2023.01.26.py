f9259b48993f56c792fcded57426c0b40a2b1801
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2023-01-26 19:25:05 +0900

    Create 2023.01.26.py

---
+++ 2023.01.26.py
@@ -0,0 +1,12 @@
+
+# Q.6007 "C:\Download\'hello'.py" 출력하기
+print("\"C:\\Download\\\'hello\'.py\"")
+# -> 문자열에 특수 기호가 들어갈 때는 맨 끝이 아니라 중간에도 특수기호 앞에 \를 붙여준다.
+
+
+# Q.6008 print("Hello\nWorld") 출력하기
+print("print(\"Hello\\nWorld\")")
+
+# Q.6009 print("Hello\nWorld") 출력하기
+print("print(\"Hello\\nWorld\")")
+
