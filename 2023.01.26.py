9cadf4aa67057738b60bbcb6af035adf61c7bd45
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2023-01-26 20:04:16 +0900

    test commit

--- 2023.01.26.py
+++ 2023.01.26.py
@@ -7,6 +7,15 @@ print("\"C:\\Download\\\'hello\'.py\"")
 # Q.6008 print("Hello\nWorld") 출력하기
 print("print(\"Hello\\nWorld\")")
 
-# Q.6009 print("Hello\nWorld") 출력하기
-print("print(\"Hello\\nWorld\")")
+# Q.6009 변수에 문자 1개를 저장한 후 변수에 저장되어 있는 문자를 그대로 출력해보자.
+# print("문자열 입력받기")
+# what = input()
+# print(what)
 
+# Q.6010 변수에 정수값을 저장한 후 정수로 변환하여 출력해보자.
+int_var = input()
+print(int(int_var))
+
+# Q.6011 변수에 실수 값을 저장한 후 정수로 변환하여 출력해보자.
+int_var = input()
+print(int(int_var))
