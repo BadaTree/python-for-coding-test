c3333d3a0f1ead526966cea0c36cb8ffd80817ec
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2023-02-21 10:38:23 +0900

    Update 2023.02.21.py

--- 2023.02.21.py
+++ 2023.02.21.py
@@ -1,12 +1,26 @@
 # 이것이 코딩 테스트다 with python, 15강 구현 문제 풀이
 
 # Q. 정수 N이 입력되면 00시 00분 00초부터 N시 59분 59초까지의 모든 시각 중에서 3이 하나라도 포함되는 모든 경우의 수를 구하는 프로그램을 작성하세요.
-hour = input()
+
+# h,m,s를 문자열로 받아와서 count하는 방식
+hour = int(input())
 count = 0
 for i in range(hour+1):
     for j in range(60):
         for k in range(60):
             if '3' in str(i)+str(j)+ str(k):
+                count += 1
+
+print(count)
+
+# h,m,s 를 숫자로 받아와서 count하는 방식
+
+hour = int(input())
+count = 0
+for i in range(hour+1):
+    for j in range(60):
+        for k in range(60):
+            if  i % 10 ==3 or j/10 == 3 or j % 10 == 3 or k/10 == 3 or k % 10 == 3:
                 count += 1
 
 print(count)
