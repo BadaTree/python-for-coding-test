a867b1f63e3b81ee7a8a6603adc241cd71f1e915
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2023-02-23 09:49:02 +0900

    Update 2022.02.23.py

--- 2022.02.23.py
+++ 2022.02.23.py
@@ -1,4 +1,6 @@
-# score = input()
+
+# 9498
+#score = input()
 # if 90<= int(score)<= 100:
 #     print("A")
 # elif 80<= int(score)<= 89:
@@ -10,14 +12,17 @@
 # else :
 #     print("F")
 
-start_h, start_m = input().split()
-start_h =int(start_h)
-start_m = int(start_m)
-time = int(input())
-
-end_time = (start_h*60) + start_m + time
-print()
-
+# 2525
+# start_h, start_m = input().split()
+# start_h =int(start_h)
+# start_m = int(start_m)
+# time = int(input())
+#
+# end_time = (start_h*60) + start_m + time
+# end_h = (end_time / 60) % 24
+# end_m = end_time % 60
+#
+# print(int(end_h),end_m)
 # end_h = (time/60) + int(start_h)
 # end_m = time % 60 + int(start_m)
 #
@@ -28,4 +33,26 @@ print()
 #     end_m = end_m% 60
 #
 # print(int(end_h),end_m)
+
+# 2048
+# a,b,c = input().split()
+# score = [int(a), int(b), int(c)]
+# if int(a) != int(b) and int(a) != int(c) and int(b) != int(c):
+#    print( max(score)*100)
+# elif (int(a) == int(b) or int(b) == int(c) or int(a)== int(c)) and not (int(a)==int(b) and int(a)==int(c)):
+#     if int(a) == int(b):
+#         print(1000+(int(a)*100))
+#     elif int(b) == int(c):
+#         print(1000+(int(b)*100))
+#     elif int(a) == int(c):
+#         print(1000+(int(a)*100))
+# elif int(a)==int(b) and int(a)==int(c):
+#     print(10000 + (int(a)*1000))
+
+# 2743
+# word = input()
+# print(len(word))
+
+# 10809
+S = input()
 
