022837d194ad1ff0060c69f01c43409a8fdf79f6
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2023-03-01 18:52:36 +0900

    Update 2023.03.01.py

--- 2023.03.01.py
+++ 2023.03.01.py
@@ -1,13 +1,21 @@
-case_num = input()
-sum_score = 0
-for i in range(int(case_num)):
-    score_list = input().split()
-    sum_score = 0
-    count = 0
-    for j in range(int(score_list[0])):
-        sum_score += int(score_list[j+1])
-    Avg_score = sum_score // int(score_list[0])
-    for j in range(int(score_list[0])):
-        if int(score_list[j+1]) >  Avg_score :
-            count +=1
-    print("{:.3f}%".format((count/int(score_list[0]))*100))
+# case_num = input()
+# sum_score = 0
+# for i in range(int(case_num)):
+#     score_list = input().split()
+#     sum_score = 0
+#     count = 0
+#     for j in range(int(score_list[0])):
+#         sum_score += int(score_list[j+1])
+#     Avg_score = sum_score // int(score_list[0])
+#     for j in range(int(score_list[0])):
+#         if int(score_list[j+1]) >  Avg_score :
+#             count +=1
+#     print("{:.3f}%".format((count/int(score_list[0]))*100))
+
+# word = input()
+# word = word[:]
+# result = 1
+# for i in range(len(word)//2):
+#     if word[i] != word[-(i+1)]:
+#         result = 0
+# print(result)
