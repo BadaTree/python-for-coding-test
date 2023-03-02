c64cd8cf7cf29a22243bc568e29e31e8b0582089
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2023-03-02 12:17:49 +0900

    Update 2023.03.02.py

--- 2023.03.02.py
+++ 2023.03.02.py
@@ -1,23 +1,20 @@
 a,b = input().split()
 
-list_1 = [[]]
-list_2 = [[]]
-final_list = [[]]
-
+list_1 = [[0 for j in range(int(a))] for i in range(int(b))]
+list_2 = [[0 for j in range(int(a))] for i in range(int(b))]
+final_list = [[0 for j in range(int(a))] for i in range(int(b))]
+temp_1 = []
+temp_2 = []
 for i in range(int(a)):
-    temp_1 = []
     temp_1 = input().split()
     for j in range(int(b)):
-        print(temp_1)
-        list_1[i][j] = int(temp_1[j])
+        value = int(temp_1[j])
+        list_1[i][j] = value
 for i in range(int(a)):
-    temp_1 = []
-    temp_2 = []
-    temp_1 = input().split()
     temp_2 = input().split()
     for j in range(int(b)):
-        print(temp_1)
-        list_1[i][j] = int(temp_1[j])
-        list_2[i][j] = int(temp_2[j])
-final_list[i][j] =  list_1[i][j] +  list_2[i][j]
-print(final_list)
+        value = int(temp_2[j])
+        list_2[i][j] = value
+        final_list[i][j] = list_1[i][j] + value
+for i in range(int(a)):
+    print(final_list[i])
