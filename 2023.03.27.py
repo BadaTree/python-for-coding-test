bcb42187ad5bf4364e5e5cafef1bc77ddbd132a3
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2023-03-27 13:32:47 +0900

    Update 2023.03.27.py

--- 2023.03.27.py
+++ 2023.03.27.py
@@ -1,9 +1,20 @@
-n = int(input())
-list = input().split()
-v = int(input())
-count = 0
-for i in range(n):
-    list[i] = int(list[i])
-    if v == list[i]:
-        count += 1
-print(count)
+# n = int(input())
+# list = input().split()
+# v = int(input())
+# count = 0
+# for i in range(n):
+#     list[i] = int(list[i])
+#     if v == list[i]:
+#         count += 1
+# print(count)
+
+N, X = input().split()
+N = int(N)
+X = int(X)
+num = input().split()
+final = []
+
+for i in range(N):
+    if int(num[i])< X:
+        final.append(int(num[i]))
+print(*final)
