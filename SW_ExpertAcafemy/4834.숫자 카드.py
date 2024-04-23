a332d4488a1d19e2ce0e4f59db9645542a568261
Bada Lim <66122916+BadaTree@users.noreply.github.com>
2024-04-23 22:33:07 +0900

    Update 4834.숫자 카드.py

--- SW_ExpertAcafemy/4834.숫자 카드.py
+++ SW_ExpertAcafemy/4834.숫자 카드.py
@@ -29,5 +29,6 @@ for i in range(1,T+1):
         cards[j] += 1
     
     max_num = max(cards)
-    
-    print(f"#{i} {cards.index(max_num)} {max_num} ")
+    print(enumerate(cards))
+    max_index = max(i for i, v in enumerate(cards) if v == max_num) 
+    print(f"#{i} {max_index} {max_num} ")
