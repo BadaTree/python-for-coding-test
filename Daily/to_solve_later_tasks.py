32e1b22500802d3de40dbc98726e3e57237676f1
BadaTree <zzzxxx3166@naver.com>
2024-05-26 11:49:44 +0900

    Update a problems

---
+++ Daily/to_solve_later_tasks.py
@@ -0,0 +1,47 @@
+# TODO : 나중에 풀어야하는 문제 리스트
+
+'''
+[ 5. 18 (토) 스터디 문제 공유 ]
+
+ 
+회전하는 큐: https://st-lab.tistory.com/216
+
+ 
+요세푸스: https://www.acmicpc.net/problem/1158
+
+ 
+암기왕: https://www.acmicpc.net/problem/2776
+
+ 
+좋은단어: https://www.acmicpc.net/problem/3986
+
+ 
+뒤집기: https://www.acmicpc.net/problem/1439
+
+ 
+단어뒤집기2: https://www.acmicpc.net/problem/17413
+
+기능 개발 : https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+[ 5. 25 (토) 스터디 문제 공유 ]
+
+제로: https://www.acmicpc.net/problem/10773
+
+도키도키 간식드리미: https://www.acmicpc.net/problem/12789
+
+최소직사각형: https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+막대기: https://www.acmicpc.net/problem/17608
+
+프린터 큐: https://www.acmicpc.net/problem/1966
+
+더 맵게: https://school.programmers.co.kr/learn/courses/30/lessons/42626
+
+달팽이: https://www.acmicpc.net/problem/1913
+
+level2문제: https://school.programmers.co.kr/learn/courses/30/lessons/147354
+level3문제: https://school.programmers.co.kr/learn/courses/30/lessons/42579
+
+폰켓몬(level1): https://school.programmers.co.kr/learn/courses/30/lessons/1845
+전화번호 목록(level2): https://school.programmers.co.kr/learn/courses/30/lessons/42577
+'''
