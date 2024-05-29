b911f2013ae44f6ef46bddc54f635f42624d9c22
BadaTree <zzzxxx3166@naver.com>
2024-05-29 17:47:54 +0900

    Update CodingStudy3.py

--- Daily/CodingStudy3.py
+++ Daily/CodingStudy3.py
@@ -53,7 +53,20 @@ plays = [500, 600, 150, 800, 2500]
 result = solution(genres, plays)
 print(result)
 
+# [ ] 완주하지 못한 선수 
 
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
 
 
 
