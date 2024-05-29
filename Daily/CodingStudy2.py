77535258f4be7067a9fc81d77eb7dc2e7a993af4
BadaTree <zzzxxx3166@naver.com>
2024-05-29 14:29:13 +0900

    Update a 05.25 note

--- Daily/CodingStudy2.py
+++ Daily/CodingStudy2.py
@@ -123,3 +123,38 @@ for _ in range(N):
             print("NO") # 괄호 순서가 잘못된 경우 예: )))(((
     else:
         print('NO') # 괄호가 쌍으로 존재하지 않는 경우 예: (()))
+        
+        
+        
+# TODO: 5.25일 스터디 문제를 복습해보자 
+
+'''
+제로: https://www.acmicpc.net/problem/10773
+
+도키도키 간식드리미: https://www.acmicpc.net/problem/12789
+
+최소직사각형: https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+막대기: https://www.acmicpc.net/problem/17608
+
+프린터 큐: https://www.acmicpc.net/problem/1966
+
+더 맵게: https://school.programmers.co.kr/learn/courses/30/lessons/42626
+
+달팽이: https://www.acmicpc.net/problem/1913
+'''
+
+
+# [1] 
+
+# [2] 
+
+# [3] 
+
+# [4]
+
+# [5]
+
+# [6]
+
+# [7]
