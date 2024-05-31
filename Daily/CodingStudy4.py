0e75ae13c2fc5871b15c16ce641f671559ba7315
BadaTree <zzzxxx3166@naver.com>
2024-05-31 15:41:43 +0900

    Add new coding test problems

---
+++ Daily/CodingStudy4.py
@@ -0,0 +1,27 @@
+# TODO : 2024.06.01 코테스터디 해쉬 관련 자유 문제 
+
+
+# [ ] 의상 (Level2, 프로그래머스)
+# https://school.programmers.co.kr/learn/courses/30/lessons/42578 
+
+
+# [ ] 메뉴 리뉴얼 (level2, 프로그래머스)
+# https://school.programmers.co.kr/learn/courses/30/lessons/72411
+
+# [ ] 패션왕 신해빈 (실버 3, 백준)
+# https://www.acmicpc.net/problem/9375
+
+
+# TODO: Hash 관련 추천 문제를 풀어보자 
+
+# [ ]나는야 포켓몬 마스터 이다솜
+# https://www.acmicpc.net/problem/1620
+
+# [ ] 듣보잡
+# https://www.acmicpc.net/problem/1764
+
+# [ ] 신고 결과 받기
+# https://school.programmers.co.kr/learn/courses/30/lessons/92334
+
+# [ ] 순위 검색
+# https://school.programmers.co.kr/learn/courses/30/lessons/72412
