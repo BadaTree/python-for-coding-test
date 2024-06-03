7db5a9aae5f2b8031ff7d85adb6c6ff77efb862a
BadaTree <zzzxxx3166@naver.com>
2024-06-03 22:33:44 +0900

    Update a problems

--- Daily/CodingStudy5.py
+++ Daily/CodingStudy5.py
@@ -1,6 +1,17 @@
 # TODO : 2024.06.05 코테스터디 해쉬 관련 자유 문제 
 
 
+# [ ] 신고 결과 받기 (Level1, 프로그래머스)
+# https://school.programmers.co.kr/learn/courses/30/lessons/92334
+
+def solution(id_list, report, k):
+    answer = []
+    return answer
+
+# [ ] 순위 검색 (Level2, 프로그래머스)
+# https://school.programmers.co.kr/learn/courses/30/lessons/72412
+
+
 # [ ] 의상 (Level2, 프로그래머스)
 # https://school.programmers.co.kr/learn/courses/30/lessons/42578 
 
@@ -13,9 +24,7 @@
 # https://www.acmicpc.net/problem/9375
 
 
-# [ ] 신고 결과 받기
-# https://school.programmers.co.kr/learn/courses/30/lessons/92334
 
 
-# [ ] 순위 검색
-# https://school.programmers.co.kr/learn/courses/30/lessons/72412
+
+
