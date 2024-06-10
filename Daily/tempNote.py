09ae2092556851c2b29ae6ad00e50367cfc3f6e8
BadaTree <zzzxxx3166@naver.com>
2024-06-10 23:22:47 +0900

    Update a problem

--- Daily/tempNote.py
+++ Daily/tempNote.py
@@ -1,20 +1,34 @@
-# TODO : 간단하게 코드 사용할 때 사용하는 메모장 노트
+# # TODO : 간단하게 코드 사용할 때 사용하는 메모장 노트
 
-# import sys
+# # import sys
 
-input = sys.stdin.read
-data = input().split()
+# input = sys.stdin.read
+# data = input().split()
 
-data = list(map(int,data[1:]))
+# data = list(map(int,data[1:]))
 
-data.sort()
+# data.sort()
 
-for i in data :
-    print(i)
+# for i in data :
+#     print(i)
 
-# 백준 1427
-N = list(input())
+# # 백준 1427
+# N = list(input())
 
-N.sort(reverse=True)
+# N.sort(reverse=True)
 
-print(''.join(N))
+# print(''.join(N))
+
+# 백준 2720
+
+N = int(input())
+moneys = [25,10,5,1]
+
+for i in range(N) :
+    pay = int(input())
+    result = []
+    for money in moneys :
+        result.append(str(int(pay / money)))
+        pay = pay % money
+  
+    print(' '.join(result))
