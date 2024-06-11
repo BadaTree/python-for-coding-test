d2719fef57b67955fee43283f1e78fe2e0b964f3
BadaTree <zzzxxx3166@naver.com>
2024-06-11 14:52:11 +0900

    Update a Tree problems

---
+++ Daily/TREE_CodingTest6.py
@@ -0,0 +1,22 @@
+# TREE에 대해 공부해 보자 
+
+# [ ] 기본 개념 강의
+# https://www.youtube.com/watch?v=ohrwGtqeW-I&t=9s
+# https://youtu.be/i57ZGhOVPcI?si=lrDWF1wZA4pLekpc
+
+# [ ] 예제 문제 풀이 
+
+# 1. 트리 만들기 (백준 14244, 실4)
+# https://www.acmicpc.net/problem/14244
+
+# 2. 세모 (백준 27966, 실3)
+# https://www.acmicpc.net/problem/27966
+
+# 3. 상근이의 여행 (백준 9372, 실4)
+# https://www.acmicpc.net/problem/9372
+
+# 4. Binary tree (백준 13237, 실3)
+# https://www.acmicpc.net/problem/13237
+
+# 5. 트리 순회 (백준 1991, 실1)
+# https://www.acmicpc.net/problem/1991
