9e26493650af1e8275e7b920b3303835c947fbab
BadaTree <zzzxxx3166@naver.com>
2024-06-12 16:15:03 +0900

    Add the Tree template

--- Daily/TREE_CodingTest6.py
+++ Daily/TREE_CodingTest6.py
@@ -20,3 +20,25 @@
 
 # 5. 트리 순회 (백준 1991, 실1)
 # https://www.acmicpc.net/problem/1991
+
+
+# [ ] Tree 개념 
+
+from collections import deque 
+
+def levelOrder(root):
+    visited = []
+    if root is None :
+        return 0
+    q = deque()
+    q.append(root)
+    while q :
+        cur_node = q.popleft()
+        visited.append(cur_node.value)
+        
+        if cur_node.left:
+            q.append(cur_node.left)
+        if cur_node.right:
+            q.append(cur_node.right)
+            
+        return visited
