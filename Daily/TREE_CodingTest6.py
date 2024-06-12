40a5950cd8b4ca0a0f2c0a19627af5d6f9654c64
BadaTree <zzzxxx3166@naver.com>
2024-06-12 17:14:01 +0900

    Update a Tree problems

--- Daily/TREE_CodingTest6.py
+++ Daily/TREE_CodingTest6.py
@@ -22,23 +22,3 @@
 # https://www.acmicpc.net/problem/1991
 
 
-# [ ] Tree 개념 
-
-from collections import deque
-
-def levelOrder(root):
-    if root is None :
-        return 0
-    visited = []
-    q = deque()
-    q.append(root)
-    
-    while q:
-        cur_node = q.popleft()
-        visited.append(cur_node.value)
-        
-        if cur_node.left:
-            q.append(cur_node.left)
-        if cur_node.right:
-            q.append(cur_node.right)
-    return visited
