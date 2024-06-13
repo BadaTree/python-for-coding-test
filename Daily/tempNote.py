e777ff40e523f31b74233907999a1b015729a5af
BadaTree <zzzxxx3166@naver.com>
2024-06-13 23:12:27 +0900

    Update a backjoon problems

--- Daily/tempNote.py
+++ Daily/tempNote.py
@@ -34,15 +34,30 @@
 #     print(' '.join(result))
 
 
+# while True:
+#     n,m = input().split(' ')
+#     n,m = int(n), int(m)
+#     if n == 0 or m == 0:
+#         break
+#     check = max(n,m) % min(n,m) == 0 
+#     if not check :
+#         print("neither")
+#     elif n < m and check:
+#         print("factor")
+#     else:
+#         print("multiple")
+
+# N,K = map(int,input().split(' '))
+# factor = [i for i in range(1,N+1) if N%i == 0]
+
+# print(factor[K-1] if len(factor)>= K else 0)
+
 while True:
-    n,m = input().split(' ')
-    n,m = int(n), int(m)
-    if n == 0 or m == 0:
+    N = int(input())
+    if N == -1 :
         break
-    check = max(n,m) % min(n,m) == 0 
-    if not check :
-        print("neither")
-    elif n < m and check:
-        print("factor")
-    else:
-        print("multiple")
+    factor = [i for i in range(1,N) if N%i == 0]
+    if N == sum(factor): # 완전수라면
+        print( f"{N} = " + " + ".join(map(str, factor)))
+    else :
+        print(f"{N} is NOT perfect.")
