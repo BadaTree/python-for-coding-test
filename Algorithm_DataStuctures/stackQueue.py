0a47301db2910a7b7ace5324fa8a0745cb39c67d
BadaTree <zzzxxx3166@naver.com>
2024-07-22 00:10:09 +0900

    Update the solution

--- Algorithm_DataStuctures/stackQueue.py
+++ Algorithm_DataStuctures/stackQueue.py
@@ -2,6 +2,30 @@
 
 # 스택, 백준 10828 
 # https://www.acmicpc.net/problem/10828
+import sys
+
+def stack_fun(num_list,command):
+    if command == "pop":
+        return num_list.pop()
+    elif command == "size":
+        return len(num_list)
+    elif command == "empty":
+        return 1 if not num_list else 0
+    elif command == "top":
+        return -1 if not num_list else num_list[-1]
+
+input = sys.stdin.read().readlines()
+N = int(input[0])
+num_list = []
+
+for i in range(1,N+1):
+    comm, num = input[i].split(' ')
+    if comm == "push":
+        num_list.append(int(num))
+    print(stack_fun(num_list,comm))
+
+
+
 
 # 단어 뒤집기, 백준 9093
 # https://www.acmicpc.net/problem/9093
