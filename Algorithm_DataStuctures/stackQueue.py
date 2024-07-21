bb4c914178e55721ba406ee930f8dab92fbf0899
BadaTree <zzzxxx3166@naver.com>
2024-07-21 23:42:46 +0900

    Add a stackQueue problems

---
+++ Algorithm_DataStuctures/stackQueue.py
@@ -0,0 +1,25 @@
+# TODO : Stack과 Queue 
+
+# 스택, 백준 10828 
+# https://www.acmicpc.net/problem/10828
+
+# 단어 뒤집기, 백준 9093
+# https://www.acmicpc.net/problem/9093
+
+# 괄호, 백준 9012
+# https://www.acmicpc.net/problem/9012
+
+# 스택 수열, 백준 1874
+# https://www.acmicpc.net/problem/1874
+
+# 에디터, 백준 1406
+# https://www.acmicpc.net/problem/1406
+
+# 큐, 백준 10845
+# https://www.acmicpc.net/problem/10845
+
+# 요세푸스, 백준 1158
+# https://www.acmicpc.net/problem/1158
+
+# 덱, 백준 108866
+# https://www.acmicpc.net/problem/10866
