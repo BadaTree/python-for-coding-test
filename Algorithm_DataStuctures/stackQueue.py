2574d190e9e854d5d128ae2737246bcd420881f7
BadaTree <zzzxxx3166@naver.com>
2024-07-22 14:59:14 +0900

    Update 백준 1406

--- Algorithm_DataStuctures/stackQueue.py
+++ Algorithm_DataStuctures/stackQueue.py
@@ -32,6 +32,7 @@ for i in range(1, N + 1):
 
 # 단어 뒤집기, 백준 9093
 # https://www.acmicpc.net/problem/9093
+'''
 import sys
 input = sys.stdin.readlines()
 
@@ -39,15 +40,98 @@ N = int(input[0])
 for i in range(1,N+1):
     words = input[i].strip().split(' ')
     print(' '.join([word[::-1] for word in words]))
-
+'''
 # 괄호, 백준 9012
 # https://www.acmicpc.net/problem/9012
+'''
+import sys
+input = sys.stdin.readlines()
+
+N = int(input[0])
+
+for i in range(1,N+1):
+    ps = input[i].strip()
+    pslist = []
+    isVps = True
+    for j in ps :
+        if j == '(':
+            pslist.append(j)
+        else :
+            if not pslist :
+                isVps = False
+                break
+            else :
+                pslist.pop()
+ 
+    print("YES" if not pslist and isVps else "NO")
+'''
 
 # 스택 수열, 백준 1874
 # https://www.acmicpc.net/problem/1874
 
+
 # 에디터, 백준 1406
 # https://www.acmicpc.net/problem/1406
+
+#내가 푼 것 
+'''
+문제점 분석
+deque의 rotate 사용: deque.rotate를 사용하여 커서를 이동하는 방식은 효율적이지 않습니다. 특히, 많은 수의 커서 이동 명령이 있을 때 비효율적입니다.
+명령어 처리 로직: 명령어 'B'에서 항상 pop을 사용하는 방식은 커서 위치에 관계없이 항상 오른쪽 끝에서 문자를 제거합니다. 이 부분이 잘못되었습니다.
+'''
+
+import sys
+from collections import deque
+
+input = sys.stdin.read().strip().splitlines()
+msg = deque(input[0])
+N = int(input[1])
+move = 0
+
+for i in range(2,N+2):
+    cmd = input[i].split(' ')
+    if cmd[0] == 'L':
+        msg.rotate(1)
+        move += -1
+    elif cmd[0] == 'D':
+        msg.rotate(-1)
+        move += 1
+    elif cmd[0] == 'B':
+        msg.pop()
+    elif cmd[0] == 'P':
+        msg.append(cmd[1])
+    print(msg)
+msg.rotate(move)
+print(''.join(msg))
+
+# GPT가 풀이
+import sys
+input = sys.stdin.read
+
+data = input().splitlines()
+initial_string = data[0]
+m = int(data[1])
+commands = data[2:]
+
+left_stack = list(initial_string)
+right_stack = []
+
+for command in commands:
+    if command == "L":
+        if left_stack:
+            right_stack.append(left_stack.pop())
+    elif command == "D":
+        if right_stack:
+            left_stack.append(right_stack.pop())
+    elif command == "B":
+        if left_stack:
+            left_stack.pop()
+    elif command.startswith("P"):
+        _, char = command.split()
+        left_stack.append(char)
+
+result = "".join(left_stack + right_stack[::-1])
+print(result)
 
 # 큐, 백준 10845
 # https://www.acmicpc.net/problem/10845
