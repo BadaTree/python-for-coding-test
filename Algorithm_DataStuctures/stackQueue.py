6484eed388e18aafa90af89180e900335a1bea14
BadaTree <zzzxxx3166@naver.com>
2024-07-23 12:30:55 +0900

    Add solution for 백준108866

--- Algorithm_DataStuctures/stackQueue.py
+++ Algorithm_DataStuctures/stackQueue.py
@@ -189,6 +189,7 @@ for i in range(1, N + 1):
         
 # 요세푸스, 백준 1158
 # https://www.acmicpc.net/problem/1158
+'''
 from collections import deque
 
 N,K = map(int,input().split(' '))
@@ -220,6 +221,39 @@ while queue:
 # 결과 출력
 print('<' + ', '.join(map(str, result)) + '>')  
 
-
+'''
 # 덱, 백준 108866
 # https://www.acmicpc.net/problem/10866
+
+from collections import deque
+import sys
+
+input = sys.stdin.readlines()
+
+N = int(input[0].strip())
+queue = deque()
+
+for i in range(1, N + 1):
+    cmd = input[i].strip().split()
+    if cmd[0] == "push_front":
+        queue.appendleft(cmd[1])
+    elif cmd[0] == "push_back":
+        queue.append(cmd[1])
+    elif cmd[0] == "pop_front":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == "pop_back":
+        if queue:
+            print(queue.pop())
+        else:
+            print(-1)
+    elif cmd[0] == "size":
+        print(len(queue))
+    elif cmd[0] == "empty":
+        print(1 if not queue else 0)
+    elif cmd[0] == "front":
+        print(queue[0] if queue else -1)
+    elif cmd[0] == "back":
+        print(queue[-1] if queue else -1)
