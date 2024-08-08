68457f5d577320958550576ed4bf838e5bffebde
BadaTree <zzzxxx3166@naver.com>
2024-08-08 21:01:38 +0900

    Add review for Sesol Tech coding test problem

--- Daily/08.08.py
+++ Daily/08.08.py
@@ -1,5 +1,28 @@
-# [ ] 코테 문제 
+# [ ] 세솔 테크 코테 문제 복기 
 
+# [ ] problem 1
+# input :
+# 주문한 메뉴 가격 [피자,치킨,보쌈, 김밥] , 할인율이 다른 쿠폰 리스트 [[50,0,0,30],[20,30,40,50]]
+# output :
+# [할인률이 가장 높은 쿠폰 번호, 할인된 총 가격]
+# [500,0,200,0] -> 피자,보쌈만 주문 
+# [500,0,0,200], [[0,20,30,0], [0,50,50,0]] 
+# -> 주문한 음식에 적용되는 할인이 없어서 쿠폰 미적용과 가격이 같음 -> [-1,-1] 반환
+
+
+
+def solution(price,coupon):
+    n = 100000000000
+    result = 10000000000
+    
+    for i in range(len(coupon)):
+        cur_price = sum([price[j]*(1-(coupon[i][j]*0.01)) for j in range(len(price))])
+        if result > cur_price :
+            n = i
+            result = int(cur_price)
+    return [n+1,result] if sum(price) != result else [-1,-1]
+
+# [ ] Problem 2
 from collections import deque
 
 
